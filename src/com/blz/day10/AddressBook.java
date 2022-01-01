74b86857e0580612ac0097b2c0762a7c57acbf82
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 20:09:53 +0530

    Address Book System

---
+++ src/com/blz/day10/AddressBook.java
@@ -0,0 +1,12 @@
+package com.blz.day10;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System !!!");
+
+	}
+
+}
